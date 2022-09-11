876c6d317ebd95735bf6fbe4b9b765ba2938c324
triviat <dimitrybely@gmail.com>
2022-09-11 15:07:38 +0300

    fix

--- hello.py
+++ hello.py
@@ -1,13 +1,9 @@
 def app(env, start_response):
     status = '200 OK'
-    headers = [('Content-Type', 'text/plain')]
+    headers = [('Content-Type', 'text/plain'),]
 
-    params = env.get('QUERY_STRING', False)
-    if params is False:
-        status = '401 No params'
-        params = 'ERROR! There is no params.'
-
-    body = params.lstrip('/?').split('&')
+    params = env['QUERY_STRING']
+    body = '\n'.join(params.lstrip('/?').split('&'))
 
     start_response(status, headers)
-    return body
+    return [bytes(body, 'ascii')]
--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo gunicorn -b 0.0.0.0:8080 hello:app
