dc04ce8ca775fe81dcb8893d531ac746dabda8e5
triviat <dimitrybely@gmail.com>
2022-09-11 16:24:08 +0300

    second step done

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
 def app(env, start_response):
     status = '200 OK'
-    headers = [('Content-Type', 'text/plain'),]
+    headers = [('Content-Type', 'text/plain')]
 
     params = env['QUERY_STRING']
     body = '\n'.join(params.lstrip('/?').split('&'))
 
     start_response(status, headers)
     return [bytes(body, 'ascii')]
+    
