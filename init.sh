8a58b0154a2c0f9df9b7699f06978997fa02e86e
triviat <dimitrybely@gmail.com>
2022-09-16 13:10:16 +0300

    ready to start django-project from gunicorn

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-available/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn -b 0.0.0.0:8080 hello:app
+gunicorn -b 0.0.0.0:8080 hello:app
+gunicorn -b 0.0.0.0:8000 ./ask/ask/wsgi:django
